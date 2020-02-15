40a23bc1a1c9f6288ef1cc2f187799e30f682918
shawn johnson <shawnjohnson@shawns-MacBook-Air.local>
2020-02-15 11:22:42 -0800

    fixed js issues with icons

--- weather.js
+++ weather.js
@@ -32,6 +32,20 @@ window.onload = function() {
             if(httpReqWeather.readyState == 4 && httpReqWeather.status == 200) {
                 const jsonWeather = JSON.parse(httpReqWeather.responseText);
                 console.log(jsonWeather);
+                const description = jsonWeather.weather[0].description;
+                const id = jsonWeather.weather[0].id;
+                const temperature = jsonWeather.main.temp;
+                const tempFaren = Math.round(1.8 * (temperature - 273) + 32);
+                const humidity = jsonWeather.main.humidity;
+                const windSpeed = jsonWeather.wind.speed;
+                const visibility = jsonWeather.visibility;
+                const desc = document.getElementById("description");
+                desc.innerHTML = `<i id="icon-desc" class="wi wi-owm-${id}"></i><p id="desc">${description}</p>`;
+                const temp = document.getElementById("temperature");
+                temp.innerHTML = `${tempFaren}<i class="wi wi-thermometer"></i>`;
+                const humidityElem = document.getElementById("humidity");
+                humidityElem.innerHTML = `${humidity}%`;
+                
             }
         }
         httpReqWeather.send();
